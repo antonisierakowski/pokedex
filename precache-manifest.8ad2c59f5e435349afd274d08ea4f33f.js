f17d684b6bd5ebf2e381b946450b38f6ed1f361c
Antoni Sierakowski <antonisierakowski@gmail.com>
2019-03-12 23:30:55 +0100

    Updates

---
+++ precache-manifest.8ad2c59f5e435349afd274d08ea4f33f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "ea392be34bce08561f71",
+    "url": "/pokedex/static/js/main.e4314f24.chunk.js"
+  },
+  {
+    "revision": "9a494faf891a76ee917e",
+    "url": "/pokedex/static/js/2.a82672b6.chunk.js"
+  },
+  {
+    "revision": "ea392be34bce08561f71",
+    "url": "/pokedex/static/css/main.2ee055b2.chunk.css"
+  },
+  {
+    "revision": "080f967249dd8a90550f9d02b86f0275",
+    "url": "/pokedex/index.html"
+  }
+];
