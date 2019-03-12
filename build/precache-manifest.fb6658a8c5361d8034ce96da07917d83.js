cb4fbc850d27d18a001743b887f0228dcf0d9568
Antoni Sierakowski <antonisierakowski@gmail.com>
2019-03-12 23:13:32 +0100

    build version

---
+++ build/precache-manifest.fb6658a8c5361d8034ce96da07917d83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99689c2fdab89f9eefa4",
+    "url": "/static/js/main.30bfe507.chunk.js"
+  },
+  {
+    "revision": "9a494faf891a76ee917e",
+    "url": "/static/js/2.a82672b6.chunk.js"
+  },
+  {
+    "revision": "99689c2fdab89f9eefa4",
+    "url": "/static/css/main.2ee055b2.chunk.css"
+  },
+  {
+    "revision": "996629495e30563d23cb00117b3cd3ff",
+    "url": "/index.html"
+  }
+];
