fc5a225c0f9dcedae0817e1013e89eb6c45f236c
Antoni Sierakowski <antonisierakowski@gmail.com>
2019-03-12 23:23:26 +0100

    still trying to get pages working

---
+++ build/precache-manifest.245082b505a5804845b2bab518f2134a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47626b25ebb85f9703fc",
+    "url": "/pokedex/build/index.html/static/js/runtime~main.82705ad9.js"
+  },
+  {
+    "revision": "94e5ca0f68346a762863",
+    "url": "/pokedex/build/index.html/static/js/main.3f1a3f71.chunk.js"
+  },
+  {
+    "revision": "9a494faf891a76ee917e",
+    "url": "/pokedex/build/index.html/static/js/2.a82672b6.chunk.js"
+  },
+  {
+    "revision": "94e5ca0f68346a762863",
+    "url": "/pokedex/build/index.html/static/css/main.2ee055b2.chunk.css"
+  },
+  {
+    "revision": "a79aeb95bf584ec20e562db9b9c0cd9c",
+    "url": "/pokedex/build/index.html/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb6658a8c5361d8034ce96da07917d83.js"
+  "/pokedex/build/index.html/precache-manifest.245082b505a5804845b2bab518f2134a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pokedex/build/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
